72877e20ef676934e2167ea4f42887c058d50f14
stiwari2022 <86172530+stiwari2022@users.noreply.github.com>
2024-02-20 13:48:46 -0500

    ddd

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-    const navbar = document.querySelector('.navbar');
-
-    toggleButton.addEventListener('click', function () {
-        navbar.classList.toggle('powderblue');
-        navbar.classList.toggle('dark');
-    });
-});
