41b3ec6f2dfdb29e862335b8942aad5b585b6ce8
stiwari2022 <86172530+stiwari2022@users.noreply.github.com>
2024-02-21 09:48:50 -0500

    hi

--- General_JS/include.js
+++ General_JS/include.js
@@ -1,13 +1,4 @@
-fetch("./header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
-
-
-fetch("./footer/footer.html")
+fetch("/Footer/footer.html")
   .then(response => {
     return response.text()
   })
