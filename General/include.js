c1b4b5ec9ca96f292bb1dd153d75d27c5713cb3b
stiwari2022 <86172530+stiwari2022@users.noreply.github.com>
2024-02-21 11:05:13 -0500

    hi

--- General/include.js
+++ General/include.js
@@ -1,3 +1,11 @@
+fetch("/Navbar/navbar.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("navbar").innerHTML = data;
+  });
+
 fetch("/Footer/footer.html")
   .then(response => {
     return response.text()
